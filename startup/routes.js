a80171910207de1150c48c400131c70b4ee8d351
nemanjagajic <nemanja.gajicru96@gmail.com>
2021-06-15 18:30:49 +0200

    exporting images implemented

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,7 @@ const categories = require('../routes/categories')
 
 module.exports = app => {
   app.use(express.json())
+  app.use('/uploads', express.static(process.cwd() + '/uploads'))
   app.use('/api/auth', auth)
   app.use('/api/recipes', recipes)
   app.use('/api/categories', categories)
